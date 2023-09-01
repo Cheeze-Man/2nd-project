9c53fbbedde42942b6388daec9fbe23357a71b4c
Cheeze-Man <pksam5046@naver.com>
2023-09-01 12:37:42 +0900

    fix : 컨플릭트 해결 및 최종 수정

--- src/pages/Positions/PositionBtn.jsx
+++ src/pages/Positions/PositionBtn.jsx
@@ -4,7 +4,6 @@ import './PositionBtn.scss';
 export default function PositionBtn({ isSelected, title, onClick }) {
   return (
     <button
-      // 실제 선택되었는지 아닌지에 대한 정보는, searchParams에서 파악해야 함
       className={`positionBtn ${isSelected ? 'clicked' : ''}`}
       onClick={onClick}
     >
